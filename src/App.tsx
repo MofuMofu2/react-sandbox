67ca500fb35c9d6f4913387f4822e9790f9669f5
MofuMofu <froakie002@gmail.com>
2022-12-27 16:03:18 +0900

    feature: #3 ルーティングを試すのでReact.js用のページ（想定）とVue.js用のページ（想定）を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          This is React.js Project.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is React.js Project.</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="{`react-page`}">to React.js Page</a>
+            </li>
+            <li>
+              <a href="{`vue-page`}">to Vue.js Page</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
