3fdf16bf54c75922b5262affb505190d2a25f2d0
MofuMofu <froakie002@gmail.com>
2023-03-23 14:31:13 +0900

    feature: 検証用のStorybook追加

---
+++ src/todo-app/TODO.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import TodoApp from "./index";
+
+export default {
+  title: "Todo",
+  component: TodoApp,
+};
+
+const Template = () => <TodoApp />;
+
+export const Todo = Template.bind({});
