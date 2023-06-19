b008607da0ade8fc73c932874d71d18f579f8122
MofuMofu <froakie002@gmail.com>
2023-06-19 21:44:29 +0900

    delete: React DOMは未使用だったため削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root, {
