37ab630107bd6af949ab99d270a882b5bc228065
MofuMofu <froakie002@gmail.com>
2023-06-21 17:27:22 +0900

    feature: Form actionのdestoryとApp.tsxのdestoryが連動しているっぽい

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Root, {
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
