2573cf602131172bc0baccd3e98ef378c34b70f4
MofuMofu <froakie002@gmail.com>
2023-06-29 14:53:48 +0900

    Not Found Dataを実施
    throw new ErrorでErrorオブジェクトを作る、という通常のJavaScript処理をloader Hooksに挟む

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -12,6 +12,12 @@ type Contact = {
 
 export async function loader({ params }: any) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
