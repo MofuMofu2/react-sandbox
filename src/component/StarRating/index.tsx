46d0317cad38a3b8d12ad7c30ea091ecb7df5924
MofuMofu <froakie002@gmail.com>
2023-08-14 17:43:09 +0900

    fix: useStateの数+1の値はまだ選択されていないのでselectedの値を動的に判断するように変更

--- src/component/StarRating/index.tsx
+++ src/component/StarRating/index.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { faStar } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import Star from "../Star";
 export default function StarRating({ totalStars = 5 }): JSX.Element {
+  const [selectedStars] = useState(3);
   return (
     <>
       {[...Array(totalStars)].map((n, i) => (
-        <Star key={i} />
+        <Star key={i} selected={selectedStars > i} />
       ))}
     </>
   );
