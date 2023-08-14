3ea6fcf464eb6b3154f260201f508f0c64045cdf
MofuMofu <froakie002@gmail.com>
2023-08-14 17:49:40 +0900

    fix: userEventはv14から破壊的変更が入っているのでバージョンアップして最新のものを使う
    userEventの方がブラウザの挙動を再現できておりこちらが推奨されている
    https://testing-library.com/docs/user-event/setup/

--- src/component/StarRating/StarRating.test.tsx
+++ src/component/StarRating/StarRating.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getAllByTitle, render } from "@testing-library/react";
 import StarRating from "./";
+import userEvent from "@testing-library/user-event";
 it("星がレンダリングされている", () => {
   const screen = render(<StarRating />);
   const stars = getAllByTitle(screen.container, "red");
@@ -8,4 +9,10 @@ it("星がレンダリングされている", () => {
 
   const greyStars = getAllByTitle(screen.container, "grey");
   expect(greyStars).toHaveLength(2);
+});
+
+it("クリックするとレーティングの数が変わる", () => {
+  // イベント検知のセットアップ
+  const event = userEvent.setup();
+  const screen = render(<StarRating />);
 });
