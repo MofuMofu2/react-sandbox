7c844addfd142d6b06d9e2b0a98f0911b87979ee
MofuMofu <froakie002@gmail.com>
2023-08-17 21:57:05 +0900

    fix; #6 クリックすると星を削除できるように子コンポーネントへイベント関連のPropsを渡す

--- src/component/Color/index.tsx
+++ src/component/Color/index.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import StarRating from "../StarRating";
 
 type ColorProps = {
+  id: string;
   title: string;
   color: string;
   rating: number;
+  onRemove: (element: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 export default function Color({ ...props }: ColorProps) {
   return (
     <section>
       <h1>{props.title}</h1>
+      <button
+        onClick={(element) => {
+          props.onRemove(element);
+        }}
+      >
+        remove
+      </button>
       <div style={{ height: 50, backgroundColor: props.color }}></div>
       <StarRating selectedStars={props.rating} />
     </section>
