647171ecfc4d20d464fc705f138042e66b6aea90
MofuMofu <froakie002@gmail.com>
2023-08-18 23:06:37 +0900

    動作確認のためUIで見えるようにした

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import colorData from "../assets/color-data.json";
 import ColorList from "../component/ColorList";
+import AddColorForm from "../component/AddColorForm";
 export default function Root() {
   const [colors, setColors] = useState(colorData);
 
@@ -12,6 +13,7 @@ export default function Root() {
   return (
     <>
       <ColorList colors={colors} onRemoveColor={removeColor} />
+      <AddColorForm onNewColor={(title, color) => console.log(title, color)} />
     </>
   );
 }
