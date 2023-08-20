f0a6a219b7ec9d8ced5bb1c0e64e7b35e0853eaa
MofuMofu <froakie002@gmail.com>
2023-08-20 15:21:52 +0900

    delete: クリックイベントを伝搬しないように変更したのでケースを消す

--- src/component/StarRating/StarRating.test.tsx
+++ src/component/StarRating/StarRating.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { getAllByTitle, render } from "@testing-library/react";
 import StarRating from "./";
 import userEvent from "@testing-library/user-event";
-import exp from "constants";
+
 it("初期状態", () => {
   const screen = render(<StarRating />);
   const stars = screen.getAllByTitle("red");
@@ -13,21 +13,4 @@ it("初期状態", () => {
   expect(greyStars).toHaveLength(2);
   // テキスト
   expect(screen.getByText("3 of 5 Stars")).toBeTruthy();
-});
-
-it("クリックするとレーティングの数とテキストが変わる", async () => {
-  // イベント検知のセットアップ
-  const event = userEvent.setup();
-  const screen = render(<StarRating />);
-
-  // クリック前の状態を確認
-  const Stars = screen.getAllByTitle("red");
-  expect(Stars).toHaveLength(3);
-
-  // クリックイベントを発火
-  await event.click(Stars[0]);
-  // クリックしたindex + 1の数だけ星が赤くなる
-  expect(screen.getAllByTitle("red")).toHaveLength(1);
-  // テキストの状態も変化する
-  expect(screen.getByText("1 of 5 Stars")).toBeTruthy();
 });
