26e92e0fbe835068360aaec1cd83fb2a8df4d898
MofuMofu <froakie002@gmail.com>
2023-08-22 22:59:42 +0900

    rootコンポーネントにコンテキストを読み込みする

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import colorData from "../assets/color-data.json";
 import ColorList from "../component/ColorList";
 import AddColorForm from "../component/AddColorForm";
+import ColorProvider from "../component/ColorProvider";
 
 export default function Root() {
   return (
-    <>
+    <ColorProvider>
       <AddColorForm />
       <ColorList />
-    </>
+    </ColorProvider>
   );
 }
