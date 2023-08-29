23076aea6cfed2bae5ef8e810cf18d67492272ec
MofuMofu <froakie002@gmail.com>
2023-08-29 15:48:56 +0900

    feature: ポーリングできるか試してみる

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Data = {
+  username: "string";
+};
 
 export default function Root() {
-  return <></>;
+  const [data, setData] = useState<Data | undefined>(undefined);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:3000/api/user");
+    const param = await response.json();
+    setData(param as Data);
+  };
+
+  useEffect(() => {
+    // 初回マウント時に実行
+    fetchData();
+
+    // 10秒ごとに実行
+    const interval = setInterval(fetchData, 10000);
+
+    // コンポーネントがアンマウントされたときに実行
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <span>{data === undefined ? "データがありません" : data.username}</span>
+  );
 }
