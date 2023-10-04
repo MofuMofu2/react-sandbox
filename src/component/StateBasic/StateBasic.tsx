076c6a4016c17179ecbfbd05122c43976900f3ba
MofuMofu <froakie002@gmail.com>
2023-10-04 22:03:00 +0900

    feature: #42 リセットも作ってみた

--- src/component/StateBasic/StateBasic.tsx
+++ src/component/StateBasic/StateBasic.tsx
@@ -10,9 +10,14 @@ export default function StateBasic({ init }: StateBasicProps) {
     setCount(count + 1);
   };
 
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div>
       <button onClick={handleClick}>カウント</button>
+      <button onClick={reset}>リセット</button>
       <p>{count}回クリックされました。</p>
     </div>
   );
