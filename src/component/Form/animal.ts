0ad93f19eef53aaef4a078bda5fcd47940fb82ec
MofuMofu <froakie002@gmail.com>
2023-10-17 19:04:21 +0900

    feature: #52 オプション選択肢用の定数を定義
    次のコードでも再利用できそうなので

---
+++ src/component/Form/animal.ts
@@ -0,0 +1,12 @@
+type Animal = {
+  name: string;
+  value: string;
+};
+
+export const animals: Animal[] = [
+  { name: "選択してください", value: "" },
+  { name: "ハムスター", value: "hamster" },
+  { name: "イヌ", value: "dog" },
+  { name: "ネコ", value: "cat" },
+  { name: "ウサギ", value: "rabbit" },
+];
