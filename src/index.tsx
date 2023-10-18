32e54029100282169e5d42d10afea78e0197e4f5
MofuMofu <froakie002@gmail.com>
2023-10-18 21:46:30 +0900

    feature: #52
    FormList.tsxでType 'HTMLOptionsCollection' can only be iterated through when using the '--downlevelIteration' flag or with a '--target' of 'es2015' or higher.ts(2802)と出てきた。これはforofがes2015以降の構文だから。GitHub Copilotに聞けるの便利。

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import FormSelect from "./component/Form/FormSelect";
+import FormList from "./component/Form/FormList";
 
 if (process.env.NODE_ENV === "development") {
   void import("./mocks/browser").then(({ worker }) => {
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<FormSelect />);
+root.render(<FormList />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
