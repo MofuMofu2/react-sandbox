e7ec553644b9bb6ea2e5865ec4e6a939c3d17e11
MofuMofu <froakie002@gmail.com>
2023-10-20 23:59:47 +0900

    feature 複数チェックボックス用の雛形追加

---
+++ src/component/Form/FormCheckMulti.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function FormCheckMulti() {
+  const [form, setForm] = useState({
+    agreement: true,
+  });
+
+  const handleFormCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.checked,
+    });
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="agreement">同意します</label>
+        <input
+          id="agreement"
+          name="agreement"
+          type="checkbox"
+          checked={form.agreement}
+          onChange={handleFormCheck}
+        />
+      </form>
+      <p>{form.agreement ? `同意する` : "同意しない"}</p>
+    </>
+  );
+}
