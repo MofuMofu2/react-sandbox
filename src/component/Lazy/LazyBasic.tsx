dd74ace263500fde239c760e667699021460479a
MofuMofu <froakie002@gmail.com>
2023-10-30 21:13:57 +0900

    feature: #57 複数表示するときは複数書けば良い

--- src/component/Lazy/LazyBasic.tsx
+++ src/component/Lazy/LazyBasic.tsx
@@ -3,11 +3,15 @@ import React, { Suspense, lazy } from "react";
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const LazyButton = lazy(() => sleep(2000).then(() => import("../Star")));
+const LazyText = lazy(() =>
+  sleep(2000).then(() => import("../MyHello/MyHello"))
+);
 
 export default function LazyBasic() {
   return (
     <Suspense fallback={<p>This is fallback.</p>}>
       <LazyButton onSelect={() => {}} />
+      <LazyText myName="mofu" />
     </Suspense>
   );
 }
