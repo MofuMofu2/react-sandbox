3ffcfa9ca781cc2e7cafe9a87fd8ac6e51b9dd49
MofuMofu <froakie002@gmail.com>
2023-11-05 20:59:03 +0900

    fix: #59 組み込んで動作させてみた

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import ProfilerBasic from "./component/Profiler/ProfilerBasic";
+import PortalBasic from "./component/Portal/PortalBasic";
 
 if (process.env.NODE_ENV === "development") {
   void import("./mocks/browser").then(({ worker }) => {
@@ -14,7 +14,12 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<ProfilerBasic />);
+root.render(
+  <>
+    <div id="dialog"></div>
+    <PortalBasic />
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
