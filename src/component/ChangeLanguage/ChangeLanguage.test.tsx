0e91a200c706bdd611e069fab15110ee3beec606
MofuMofu <froakie002@gmail.com>
2023-11-06 12:40:45 +0900

    fix: beforeEachでセットアップする

--- src/component/ChangeLanguage/ChangeLanguage.test.tsx
+++ src/component/ChangeLanguage/ChangeLanguage.test.tsx
@@ -4,27 +4,29 @@ import { render } from "@testing-library/react";
 import i18n from "../../i18n";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        welcome: "Welcome to React",
-        // 他の翻訳もここに追加します
+beforeEach(() => {
+  i18n.use(initReactI18next).init({
+    resources: {
+      en: {
+        translation: {
+          welcome: "Welcome to React and react-i18next!",
+          // 他の翻訳もここに追加します
+        },
       },
+      // 他の言語の翻訳もここに追加します
     },
-    // 他の言語の翻訳もここに追加します
-  },
-  lng: "en",
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
-jest.mock("react-i18next", () => ({
-  useTranslation: () => ({ t: (key: string) => key }),
-  withTranslation: () => (Component: ComponentType) => Component,
-}));
+  jest.mock("react-i18next", () => ({
+    useTranslation: () => ({ t: (key: string) => key }),
+    withTranslation: () => (Component: ComponentType) => Component,
+  }));
+});
 
 describe("英語ロケールenを指定したとき", () => {
   it("Welcome to Reactが表示される", () => {
