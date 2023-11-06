ea771d20d8d95f4e606f6fa5d9f6cdec55762d4c
MofuMofu <froakie002@gmail.com>
2023-11-06 11:09:34 +0900

    feature: i18n定義ファイルの追加

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  en: {
+    translation: {
+      welcome: "Welcome to React and react-i18next!",
+    },
+  },
+  ja: {
+    translation: {
+      welcome: "Reactとreact-i18nextの世界へようこそ！",
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
