e8c7c035392d38f4cc5c50be0ed578d02738c4ec
MofuMofu <froakie002@gmail.com>
2023-11-11 17:41:00 +0900

    fix: MofuMofu2/rimarimadan-blog#14 複数ファイルを繰り返しフォーマットする

--- scripts/sort-json.js
+++ scripts/sort-json.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
 const prettier = require('prettier');
 
-// 翻訳ファイルのパスを指定する
-const FILE_PATH = './src/i18n/translation/ja.json';
-
-// ファイルを読み込む
-const rawJson = fs.readFileSync(FILE_PATH, 'utf8');
-const json = JSON.parse(rawJson);
+// 翻訳ファイルのパスを配列で指定する
+const FILE_PATHS = ['./src/i18n/translation/ja.json', './src/i18n/translation/en.json'];
 
 // オブジェクトのキーをソートする関数
 const sortObjectKeys = (unsortedObj) => {
@@ -18,13 +14,19 @@ const sortObjectKeys = (unsortedObj) => {
     }, {});
 }
 
-// キーをソート
-const sortedJson = sortObjectKeys(json);
+for (const FILE_PATH of FILE_PATHS) {
+    // ファイルを読み込む
+    const rawJson = fs.readFileSync(FILE_PATH, 'utf8');
+    const json = JSON.parse(rawJson);
+  
+    // キーをソート
+    const sortedJson = sortObjectKeys(json);
+  
+    // Prettierでフォーマット
+    const formattedJson = prettier.format(JSON.stringify(sortedJson), { parser: 'json' });
 
-// Prettierでフォーマット
-const formattedJson = prettier.format(JSON.stringify(sortedJson), { parser: 'json' });
-
-// Promiseを返すので、thenで書き込みを実行する
-formattedJson.then(data => {
-  fs.writeFileSync(FILE_PATH, data)
-})
+    // JSONのフォーマット完了を待ってからファイルに書き込む
+    formattedJson.then((formattedJson) => {
+        fs.writeFileSync(FILE_PATH, formattedJson);
+    });
+}
