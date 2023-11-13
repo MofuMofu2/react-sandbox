3abf5e86da0f0f7f374f4ca1027abf0c929e94ac
MofuMofu <froakie002@gmail.com>
2023-11-13 21:31:50 +0900

    feature: #67 setInterval()で生成したidは別物なのでカウンターが止まらない

--- src/component/Hooks/useRef/HookRefNg.test.tsx
+++ src/component/Hooks/useRef/HookRefNg.test.tsx
@@ -15,4 +15,14 @@ it("ボタンをクリックすると1秒ずつ待ち、開始ボタンを押す
   const element = await screen.findByText("1秒経過");
   // countの値とテキストを組み合わせた形でレンダリングされていることをテストする
   expect(element).toBeInTheDocument();
+
+  // 終了ボタンをクリック
+  await event.click(screen.getByText("終了"));
+  // Reactのstate更新が完了するのを待つ
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  });
+  const end = await screen.findByText("1秒経過");
+  // countが止まっていることをテストする
+  expect(end).toBeInTheDocument();
 });
