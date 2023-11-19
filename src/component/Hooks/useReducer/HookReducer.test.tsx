3f70eb8f37e6e697bc9fef9ecadee1c18f6f19de
MofuMofu <froakie002@gmail.com>
2023-11-19 20:37:06 +0900

    feature: #68 クリックした後のテストを追加
    handlerとstateが一体になっているのでテストの書き方は変わらない

--- src/component/Hooks/useReducer/HookReducer.test.tsx
+++ src/component/Hooks/useReducer/HookReducer.test.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import HookReducer from "./HookReducer";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 it("初期状態はpropsの値でpタグのテキストがレンダリングされる", () => {
   const screen = render(<HookReducer init={0} />);
   expect(screen.getByText("0回クリックされました")).toBeInTheDocument();
+});
+
+it("クリックすると+1した値でpタグのテキストがレンダリングされる", async () => {
+  // イベント検知のセットアップ
+  const event = userEvent.setup();
+  const screen = render(<HookReducer init={0} />);
+  // ボタンをクリックする
+  const button = screen.getByText("Count");
+  await event.click(button);
+
+  expect(screen.getByText("1回クリックされました")).toBeInTheDocument();
 });
