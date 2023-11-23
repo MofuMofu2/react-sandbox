dc96a400c4e6b54981fa734c17c2df39b3e4546b
MofuMofu <froakie002@gmail.com>
2023-11-23 17:28:01 +0900

    faeture: #69 動かしてみる

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import HookReducerInit from "./component/Hooks/useReducer/HookReducerInit";
+import HookContext from "./component/Hooks/Context/HookContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <>
-    <HookReducerInit init={0} />
+    <HookContext />
   </>
 );
 
