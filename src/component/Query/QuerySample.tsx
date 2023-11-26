9a4129941d7fb2c3b7dc7aef45a1ea2d4ea7f2ee
MofuMofu <froakie002@gmail.com>
2023-11-26 12:53:51 +0900

    feature: #48 テストを書くためAPIレスポンスの取得を試す

--- src/component/Query/QuerySample.tsx
+++ src/component/Query/QuerySample.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type User = {
+  username: string;
+  locale: string;
+};
 
 export default function QuerySample() {
+  const [users, setUsers] = useState<User | undefined>(undefined);
+
+  useEffect(() => {
+    fetch("/user")
+      .then(async (response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return await response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>QuerySample</h1>
+      <p>{users != null ? users.username : ""}</p>
+      <p>{users != null ? users.locale : ""}</p>
     </div>
   );
 }
