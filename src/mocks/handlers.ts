d70ab729e23c532427627781165bc2b0fb436174
MofuMofu <froakie002@gmail.com>
2023-11-26 12:32:18 +0900

    fix: MofuMofu2/rimarimadan-blog#23 ステータスコードも付与する例

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,9 +3,12 @@ import { HttpResponse, http } from "msw";
 export const handlers = [
   // ユーザー情報の取得用のモック
   http.get("/user", () => {
-    return HttpResponse.json({
-      username: "miff",
-      locale: "en",
-    });
+    return HttpResponse.json(
+      {
+        username: "miff",
+        locale: "en",
+      },
+      { status: 200 }
+    );
   }),
 ];
