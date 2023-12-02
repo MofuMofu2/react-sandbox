28040a5d1dc1392aed6415fb097e84ff1c986a89
MofuMofu <froakie002@gmail.com>
2023-12-02 16:40:20 +0900

    feature: #70 TODOリスト用のステートを追加

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -4,3 +4,18 @@ export const counterAtom = atom({
   key: "counterAtom",
   default: 0,
 });
+
+type Todo = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+export const todoAtom = atom<Todo[]>({
+  key: "todosAtom",
+  default: [
+    { id: 1, title: "Learn React", isDone: false },
+    { id: 2, title: "Learn Recoil", isDone: false },
+    { id: 3, title: "Learn Redux", isDone: false },
+  ],
+});
