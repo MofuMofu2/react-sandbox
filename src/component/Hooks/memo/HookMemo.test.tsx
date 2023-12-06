d640b06fe997bd03f28fb63d19515e106524efbe
MofuMofu <froakie002@gmail.com>
2023-12-06 22:50:10 +0900

    feature: #71 コンポーネントの雛形をコミット

---
+++ src/component/Hooks/memo/HookMemo.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import HookMemo from "./HookMemo";
+
+describe("HookMemo", () => {
+  it("renders HookMemo component", () => {
+    render(<HookMemo />);
+  });
+});
---
+++ src/component/Hooks/memo/HookMemo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HookMemo() {
+  return (
+    <div>
+      <h1>HookMemo</h1>
+    </div>
+  );
+}
