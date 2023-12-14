21db605b3ff28a6bb5e872fb21af130645835e25
MofuMofu <froakie002@gmail.com>
2023-12-14 20:25:30 +0900

    feature: #71 メモ化する前にインクリメント・デクリメントするコードを追加

--- src/component/Hooks/memo/HookMemo.tsx
+++ src/component/Hooks/memo/HookMemo.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+const sleep = (delay: number) => {
+  const start = Date.now();
+  while (Date.now() - start < delay);
+};
 
 export default function HookMemo() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const decrement = () => {
+    setCount2((c) => c - 1);
+  };
+
+  const heavyFunction = () => {
+    sleep(3000);
+    return count;
+  };
+
   return (
     <div>
-      <h1>HookMemo</h1>
+      <h1>Hook Memo</h1>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+      <span>{count2}</span>
+      <div>{heavyFunction()}</div>
     </div>
   );
 }
