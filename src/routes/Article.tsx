41b582f184000825d9c2974c1456c80dbb2e1071
MofuMofu <froakie002@gmail.com>
2023-12-24 20:12:57 +0900

    feature: #47 初期値をセット

--- src/routes/Article.tsx
+++ src/routes/Article.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function Article() {
-  const params = useParams();
-  return <div>{params.id != null ? "始まり" : params.id} のページです</div>;
+  const { id = "最初" } = useParams();
+  return <div>{id} のページです</div>;
 }
