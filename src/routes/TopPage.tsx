d72a413d1ad7ba50dbb7a9cbee29764498764ef9
MofuMofu <froakie002@gmail.com>
2023-12-27 18:58:21 +0900

    feature: #47 コンテキストを使って上位ルートにアクセスするサンプル
    型の付け方はいまいちわからん

--- src/routes/TopPage.tsx
+++ src/routes/TopPage.tsx
@@ -1,4 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+
+type UseOutletContextType = () => [
+  unknown,
+  (update: (c: number) => number) => void,
+];
+// 型を明示的に指定
+const useOutletContextTyped: UseOutletContextType = useOutletContext;
+
 export default function TopPage() {
+  const [, setCount] = useOutletContextTyped();
+  useEffect(() => {
+    setCount((c) => c + 1);
+  }, [setCount]);
   return <div>TopPage</div>;
 }
