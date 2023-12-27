bafa5746c768dffbbd7c77536de526180c684353
MofuMofu <froakie002@gmail.com>
2023-12-27 19:01:17 +0900

    feature: #47 トップページだとリダイレクトで0の状態に切り戻していてあまり意味がないので切り戻す

--- src/routes/TopPage.tsx
+++ src/routes/TopPage.tsx
@@ -1,17 +1,4 @@
-import React, { useEffect } from "react";
-import { useOutletContext } from "react-router-dom";
-
-type UseOutletContextType = () => [
-  unknown,
-  (update: (c: number) => number) => void,
-];
-// 型を明示的に指定
-const useOutletContextTyped: UseOutletContextType = useOutletContext;
-
+import React from "react";
 export default function TopPage() {
-  const [, setCount] = useOutletContextTyped();
-  useEffect(() => {
-    setCount((c) => c + 1);
-  }, [setCount]);
   return <div>TopPage</div>;
 }
