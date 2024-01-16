23bc99b4e1d821d8661aab6715134da4d5fe9d03
MofuMofu2 <froakie002@gmail.com>
2024-01-16 21:19:37 +0900

    feature: #98 今の実装に対してテストを追加

---
+++ src/routes/Article.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Article from "./Article";
+import { NameContext } from "./RouterApp";
+
+describe("初期状態のテスト", () => {
+  // Contextのモックを作成
+  const mockSetName = jest.fn();
+  const mockName = "";
+  it("ステートが存在する場合はステート + 固定テキストがレンダリングされていること", () => {
+    const content = render(
+      <NameContext.Provider value={{ name: mockName, setName: mockSetName }}>
+        <Article />
+      </NameContext.Provider>
+    );
+    expect(content.getByText("最初のページです")).toBeInTheDocument();
+  });
+});
--- src/routes/Article.tsx
+++ src/routes/Article.tsx
@@ -3,5 +3,5 @@ import { useParams } from "react-router-dom";
 
 export default function Article() {
   const { id = "最初" } = useParams();
-  return <div>{id} のページです</div>;
+  return <div>{id}のページです</div>;
 }
