70f65981ac7a10cb89ff1de5305dda2aa57ca4c7
MofuMofu2 <froakie002@gmail.com>
2024-01-16 15:52:29 +0900

    feature: #98 Contextを使ったテストを書くときはモックアップが必要

--- src/routes/About.test.tsx
+++ src/routes/About.test.tsx
@@ -2,20 +2,37 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import About from "./About";
+import { NameContext } from "./RouterApp";
 
 describe("初期状態のテスト", () => {
-  const content = render(<About />);
+  // Contextのモックを作成
+  const mockSetName = jest.fn();
+  const mockName = "";
+
+  const content = render(
+    <NameContext.Provider value={{ name: mockName, setName: mockSetName }}>
+      <About />
+    </NameContext.Provider>
+  );
   it("h1タグがレンダリングされていること", () => {
     expect(content.getByText("About")).toBeInTheDocument();
   });
 
   it("初期状態では名前テキストはレンダリングされていない", () => {
-    const content = render(<About />);
+    const content = render(
+      <NameContext.Provider value={{ name: mockName, setName: mockSetName }}>
+        <About />
+      </NameContext.Provider>
+    );
     expect(content.queryByText("mofu")).toBeNull();
   });
 
   it("初期状態ではフォーム内の値は存在しない", () => {
-    const content = render(<About />);
+    const content = render(
+      <NameContext.Provider value={{ name: mockName, setName: mockSetName }}>
+        <About />
+      </NameContext.Provider>
+    );
     expect(content.getByLabelText("Type Your Name")).toHaveValue("");
   });
 });
