18ca6dbfa8b154400e24c8183b0fddb58d8de443
MofuMofu2 <froakie002@gmail.com>
2024-01-18 12:49:02 +0900

    feature: MofuMofu2/rimarimadan-blog#58  テストケース用に仮実装を追加
    テストケースを書きながらコードをアップデートする

--- src/component/ChangeIcon/index.tsx
+++ src/component/ChangeIcon/index.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { faMusic, faImage, faTent } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ChangeIcon() {
+  const [icon, setIcon] = useState({ value: faMusic, text: "音楽" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    if (value === "music") {
+      setIcon({ value: faMusic, text: "音楽" });
+    } else if (value === "image") {
+      setIcon({ value: faImage, text: "画像" });
+    } else if (value === "tent") {
+      setIcon({ value: faTent, text: "テント" });
+    }
+  };
+
   return (
     <section>
       <h2>アイコンを選ぶ</h2>
-      <div className="icon">
-        <FontAwesomeIcon icon={faMusic} />
-        <FontAwesomeIcon icon={faImage} />
-        <FontAwesomeIcon icon={faTent} />
+      <select onChange={handleChange}>
+        <option value="music">音楽</option>
+        <option value="image">画像</option>
+        <option value="tent">テント</option>
+      </select>
+      <div>
+        <span>選択しているアイコンは{icon.text}です</span>
+        <FontAwesomeIcon icon={icon.value} />
       </div>
     </section>
   );
