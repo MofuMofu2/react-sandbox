815e7a7200b6ae46e4ab3bdd55fc9391334da81b
MofuMofu2 <froakie002@gmail.com>
2024-01-18 18:55:30 +0900

    fix: MofuMofu2/rimarimadan-blog#58 複数クエリを割り当ててテストを通した

--- src/component/ChangeIcon/ChangeIcon.test.tsx
+++ src/component/ChangeIcon/ChangeIcon.test.tsx
@@ -3,8 +3,8 @@ import { render } from "@testing-library/react";
 
 import ChangeIcon from ".";
 
-it("h2タグが存在する", () => {
+it("h2タグが2つ存在する", () => {
   const content = render(<ChangeIcon />);
   // h2タグが2つ存在することを確認
-  expect(content.getByRole("heading", { level: 2 })).toBeInTheDocument();
+  expect(content.getAllByRole("heading", { level: 2 })).toHaveLength(2);
 });
