e137ab4f79e3d520eb4ca6468fe86e9e7ea0308c
MofuMofu2 <froakie002@gmail.com>
2024-01-22 12:49:06 +0900

    feature: MofuMofu2/rimarimadan-blog#10 ドロップダウン選択肢の変更

--- src/component/ChangeIcon/ChangeIcon.test.tsx
+++ src/component/ChangeIcon/ChangeIcon.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-
+import userEvent from "@testing-library/user-event";
 import ChangeIcon from ".";
 
 it("h2タグが2つ存在する", () => {
@@ -11,6 +11,16 @@ it("h2タグが2つ存在する", () => {
 
 it("アイコン要素が存在することを確認する", () => {
   const content = render(<ChangeIcon />);
+  // アイコン要素が存在することを確認
+  expect(content.getByTestId("icon")).toBeInTheDocument();
+});
+
+it("ドロップダウンの値を変更するとアイコンが変わる", async () => {
+  const event = userEvent.setup();
+  const content = render(<ChangeIcon />);
+
+  // ドロップダウンの値を変更
+  await event.selectOptions(content.getByRole("combobox"), "image");
   // アイコン要素が存在することを確認
   expect(content.getByTestId("icon")).toBeInTheDocument();
 });
