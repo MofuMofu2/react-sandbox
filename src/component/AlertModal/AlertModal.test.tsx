cf5fef6801ae6567d3469711c36edd5e8d4f98e8
MofuMofu2 <froakie002@gmail.com>
2024-02-02 16:01:11 +0900

    feature: MofuMofu2/rimarimadan-blog#68 CSSで切り替える系はそもそも表示・非表示切り替えるのしんどいなあと思った。
    これはこれでブログに切り出すとしても、実際は気をつけポイントになりそう。
    レンダリングされちゃうんだもん。

--- src/component/AlertModal/AlertModal.test.tsx
+++ src/component/AlertModal/AlertModal.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import AlertModal from "./index";
 
 describe("アラートコンポーネント要素に関するテスト", () => {
@@ -17,5 +18,17 @@ describe("アラートコンポーネント要素に関するテスト", () => {
     const content = render(<AlertModal open={false} />);
     // HTML側の切り替えになるのでレンダリングはされている。
     expect(content.queryByText("アラートテキストを表示")).toBeTruthy();
+  });
+
+  it("ボタンクリックでイベント発火すること", async () => {
+    const event = userEvent.setup();
+    // イベントハンドラーのモックアップ
+    const handleEvent = jest.fn();
+    const content = render(<AlertModal onButtonClick={handleEvent} />);
+
+    // OKボタンをクリック
+    await event.click(content.getByRole("button", { name: "OK" }));
+    // イベント発火すること
+    expect(handleEvent).toHaveBeenCalled();
   });
 });
--- src/component/AlertModal/index.tsx
+++ src/component/AlertModal/index.tsx
@@ -5,13 +5,19 @@ type AlertModalProps = {
    * dialogを切り替える
    */
   open?: boolean;
+  /**
+   * ボタンコンポーネントのイベント発火を検証する
+   */
+  onButtonClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-export default function AlertModal({ open = true }: AlertModalProps) {
+export default function AlertModal({ open = true, ...props }: AlertModalProps) {
   return (
     <dialog open={open}>
       <p>アラートテキストを表示</p>
-      <button type="button">OK</button>
+      <button type="button" onClick={props.onButtonClick}>
+        OK
+      </button>
     </dialog>
   );
 }
