5d09e162e9ab5e6dae2bc393aa189254e5e5decd
MofuMofu2 <froakie002@gmail.com>
2024-02-02 15:38:43 +0900

    feature: #68 コンポーネントに組み込む

--- src/routes/alert-page/AlertPage.tsx
+++ src/routes/alert-page/AlertPage.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import AlertModal from "../../component/AlertModal";
 
 export default function AlertPage() {
-  return <div>ボタンクリックでモーダルダイアログ表示を切り替える</div>;
+  return (
+    <>
+      <h3>ボタンクリックでモーダルダイアログ表示を切り替える</h3>
+      <AlertModal />
+    </>
+  );
 }
