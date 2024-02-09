268db5d4ddb78b2cf55b2ea3222af919654c979f
MofuMofu2 <froakie002@gmail.com>
2024-02-09 23:08:18 +0900

    feature: MofuMofu2/rimarimadan-blog#65 選択後のテストを追加

--- src/component/StayExtensionOptionsRadio/StayExtensionOptionsRadio.test.tsx
+++ src/component/StayExtensionOptionsRadio/StayExtensionOptionsRadio.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import StayExtensionOptionsRadio from "./StayExtensionOptionsRadio";
 
 describe("初期状態のテスト", () => {
@@ -21,7 +22,25 @@ describe("初期状態のテスト", () => {
 describe("クリックしたとき選択状態が切り替わる", () => {
   it("最初は「宿泊なし」が選択されている", () => {
     const content = render(<StayExtensionOptionsRadio />);
-    const radio = content.getByRole("radio", { name: "宿泊なし" });
-    expect(radio).toBeChecked();
+    expect(content.getByRole("radio", { name: "宿泊なし" })).toBeChecked();
+  });
+
+  it("最初は「宿泊なし」以外は選択されていない", () => {
+    const content = render(<StayExtensionOptionsRadio />);
+    expect(content.getByRole("radio", { name: "前泊のみ" })).not.toBeChecked();
+  });
+
+  it("別の選択肢をクリックすると選択状態が切り替わる", async () => {
+    // イベント検知のセットアップ
+    const event = userEvent.setup();
+    const content = render(<StayExtensionOptionsRadio />);
+
+    // 「前泊のみ」をクリック
+    await event.click(content.getByRole("radio", { name: "前泊のみ" }));
+    // 「前泊のみ」が選択されている
+    expect(content.getByRole("radio", { name: "前泊のみ" })).toBeChecked();
+
+    // 「宿泊なし」は選択されていない
+    expect(content.getByRole("radio", { name: "宿泊なし" })).not.toBeChecked();
   });
 });
