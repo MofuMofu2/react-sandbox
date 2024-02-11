3e91a029b6914700bbd30350e902dc3ea4260bdf
MofuMofu2 <froakie002@gmail.com>
2024-02-11 16:46:53 +0900

    feature: MofuMofu2/rimarimadan-blog#66 useStateでコントロールするようにした
    他のフォームと連動させることを見据えてつけておく

--- src/component/AgreementCheckbox/AgreementCheckbox.tsx
+++ src/component/AgreementCheckbox/AgreementCheckbox.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AgreementCheckbox() {
+  const [checked, setChecked] = useState(false);
+
+  const handleChecked = () => {
+    setChecked(!checked);
+  };
+
   return (
     <div>
-      <input type="checkbox" name="agreement" />
+      <input
+        type="checkbox"
+        name="agreement"
+        onClick={handleChecked}
+        checked={checked}
+      />
       <label htmlFor="agreement">同意する</label>
     </div>
   );
