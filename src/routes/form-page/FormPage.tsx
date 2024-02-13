f9fa4d2247fe48cfcd8f2d419a85235be4453115
MofuMofu2 <froakie002@gmail.com>
2024-02-13 22:04:41 +0900

    feature: MofuMofu2/rimarimadan-blog#67 動作確認用に見えるようにした

--- src/routes/form-page/FormPage.tsx
+++ src/routes/form-page/FormPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AgreementCheckbox from "../../component/AgreementCheckbox/AgreementCheckbox";
 import StayExtensionOptionsRadio from "../../component/StayExtensionOptionsRadio/StayExtensionOptionsRadio";
+import StayExtensionSelectBox from "../../component/StayExtensionSelectBox/StayExtensionSelectBox";
 
 export default function FormPage() {
   return (
     <>
       <h3>フォームページ</h3>
       <StayExtensionOptionsRadio />
+      <StayExtensionSelectBox />
       <AgreementCheckbox />
     </>
   );
