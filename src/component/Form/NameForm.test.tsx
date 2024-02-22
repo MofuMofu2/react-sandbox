c1cb307c65b4ca913c70335cae806582364a18f4
MofuMofu2 <froakie002@gmail.com>
2024-02-22 21:09:12 +0900

    feature: MofuMofu2/rimarimadan-blog#64 値を更新したときのテストを書く
    これはwaitForで反映しないと落ちる

--- src/component/Form/NameForm.test.tsx
+++ src/component/Form/NameForm.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import StateForm from "./NameForm";
 
 describe("初期表示に関するテスト", () => {
@@ -11,5 +12,20 @@ describe("初期表示に関するテスト", () => {
   it("名前のフォームには太郎と入力されている", () => {
     const content = render(<StateForm />);
     expect(content.getByRole("textbox", { name: "名前" })).toHaveValue("太郎");
+  });
+});
+
+describe("フォーム入力値を編集したときのテスト", () => {
+  it("苗字のフォームを編集すると入力したときの値でテキストが表示される", async () => {
+    const event = userEvent.setup();
+    const content = render(<StateForm />);
+
+    // 苗字のフォームを編集する
+    await event.type(content.getByRole("textbox", { name: "苗字" }), "田中");
+
+    // 苗字のフォームを編集したときの値でテキストが表示される
+    expect(
+      content.getByText("こんにちは、田中 太郎さん！")
+    ).toBeInTheDocument();
   });
 });
