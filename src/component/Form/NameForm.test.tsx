6bac3f90bc8f1215c223666b84f6ca4d386b0998
MofuMofu2 <froakie002@gmail.com>
2024-02-22 21:06:23 +0900

    feature: MofuMofu2/rimarimadan-blog#64 初期状態のテストを追加

--- src/component/Form/NameForm.test.tsx
+++ src/component/Form/NameForm.test.tsx
@@ -7,4 +7,9 @@ describe("初期表示に関するテスト", () => {
     const content = render(<StateForm />);
     expect(content.getByRole("textbox", { name: "苗字" })).toHaveValue("山田");
   });
+
+  it("名前のフォームには太郎と入力されている", () => {
+    const content = render(<StateForm />);
+    expect(content.getByRole("textbox", { name: "名前" })).toHaveValue("太郎");
+  });
 });
