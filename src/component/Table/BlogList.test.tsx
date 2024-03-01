25179541b1bdb28bacc1b64d829123bb4fca7008
MofuMofu2 <froakie002@gmail.com>
2024-03-01 20:32:10 +0900

    feature: MofuMofu2/rimarimadan-blog#69 roleとテキストで取得するパターンを追加

--- src/component/Table/BlogList.test.tsx
+++ src/component/Table/BlogList.test.tsx
@@ -3,12 +3,21 @@ import { render } from "@testing-library/react";
 import BlogList from "./BlogList";
 import { blogPostsMock, usersMock } from "./tableMock";
 
-describe("テーブルが表示できること", () => {
-  it("テーブルヘッダーが表示できる", () => {
-    expect(
-      render(
-        <BlogList blogList={blogPostsMock} userList={usersMock} />
-      ).getByText("記事ID")
-    ).toBeInTheDocument();
+describe("テーブルヘッダーが表示できる", () => {
+  it("テーブルヘッダーのリストがデグレードしていない", () => {
+    const content = render(
+      <BlogList blogList={blogPostsMock} userList={usersMock} />
+    );
+    // テキストが存在するか確認
+    expect(content.getByText("記事ID")).toBeInTheDocument();
+
+    // ARIA-Role属性でヘッダーが取得できるか確認
+    const headers = content.getAllByRole("columnheader");
+
+    // 見出し「タイトル」が存在するか確認
+    expect(headers[1]).toHaveTextContent("タイトル");
+
+    // 見出し「投稿ユーザー」が存在するか確認
+    expect(headers[2]).toHaveTextContent("投稿ユーザー");
   });
 });
