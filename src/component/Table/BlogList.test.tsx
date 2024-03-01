9710f23391e9c0d18c2a77e57055ba7182ed7d23
MofuMofu2 <froakie002@gmail.com>
2024-03-01 20:25:46 +0900

    feature: MofuMofu2/rimarimadan-blog#69 propsを追加したのでテストケースにも反映する

--- src/component/Table/BlogList.test.tsx
+++ src/component/Table/BlogList.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import BlogList from "./BlogList";
+import { blogPostsMock, usersMock } from "./tableMock";
 
 describe("テーブルが表示できること", () => {
   it("テーブルヘッダーが表示できる", () => {
-    expect(render(<BlogList />).getByText("記事ID")).toBeInTheDocument();
+    expect(
+      render(
+        <BlogList blogList={blogPostsMock} userList={usersMock} />
+      ).getByText("記事ID")
+    ).toBeInTheDocument();
   });
 });
