792472aef1084e4be66a3194166d2f9804ae70cd
MofuMofu2 <froakie002@gmail.com>
2024-03-21 17:22:56 +0900

    feature: MofuMofu2/rimarimadan-blog#89 テストを書くためにAPIリクエストのサンプルを追加

--- src/routes/user-page/UserPage.tsx
+++ src/routes/user-page/UserPage.tsx
@@ -1,9 +1,4 @@
-import React, { useEffect, useState } from "react";
-
-import { useTranslation } from "react-i18next";
-
-import i18n from "../../i18n/i18n";
-
+import React, { useState } from "react";
 interface Data {
   username: "string";
   locale: "ja" | "en";
@@ -11,30 +6,39 @@ interface Data {
 
 export default function UserPage() {
   const [data, setData] = useState<Data | undefined>(undefined);
-  const { t } = useTranslation();
 
-  const fetchData = async () => {
-    try {
-      // この結果は子コンポーネントにPropsしつつ、i18n定義も更新したい。
-      const response = await fetch("http://localhost:3000/user");
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      if (response.ok) {
-        const param = await response.json();
-        setData(param as Data);
-        await i18n.changeLanguage(param.locale);
-      }
-    } catch (error) {
-      setData(undefined);
-      console.error(error);
-    }
+  const handleClick = () => {
+    fetchData()
+      .then((response) => {
+        if (response.ok) {
+          void response.json().then((json) => {
+            setData(json);
+          });
+        }
+      })
+      .catch((error) => {
+        setData(undefined);
+        console.error("Error:", error);
+      });
   };
 
-  useEffect(() => {
-    // 初回マウント時に実行
-    void fetchData();
-  }, []);
+  const fetchData = async () => {
+    return await fetch(`${window.location.origin}/user`);
+  };
 
-  return <span>{data === undefined ? t("nodata") : t("welcome")}</span>;
+  return (
+    <>
+      <div>
+        {data !== undefined ? (
+          <div>
+            <div>Username: {data.username}</div>
+            <div>Locale: {data.locale}</div>
+          </div>
+        ) : (
+          <div>No data</div>
+        )}
+      </div>
+      <button onClick={handleClick}>Fetch Data</button>
+    </>
+  );
 }
